1146cfb9f384bd617fff147974f1ab11f06c5ac2
Abdul Muheeth <abdulmuheeth88@gmail.com>
2023-12-31 03:27:41 +0530

    first route added

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes Import
+import userRouter from './routes/user.routes.js'
+
+// Routes Declaration
+app.use("/api/users", userRouter)
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,9 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
-
-export { asyncHandler }
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
 
 // const asyncHandler = (requestHandler) => () => async(req, res, next) => {
 //     try {
